a949fe706a4f07d9218b27a9d56afd3078a43c58
Marcin Spławiński <marcin.cincin9010@gmail.com>
2020-09-21 10:39:40 +0200

    slider - added set timeout for autoslide

--- scripts/Slider.js
+++ scripts/Slider.js
@@ -5,15 +5,39 @@ const previousButtonSelector = '[data-previous-button]';
 const nextButtonSelector = '[data-next-button]';
 const imagesToSlide = ['./images/corona-4916954_960_720.jpeg','./images/stayhome.jpeg', './images/corona-virus-5251013_960_720.jpeg', './images/quarantine.jpeg'];
 let index = 0;
+let intervalIndex = null;
+let canAutomaticallyChange = true;
+
 
 class Slider extends Common{
     constructor(){
         super(sliderImageSelector);
+        this.element.style.backgroundImage = `url(${imagesToSlide[index]})`
         const nextSlideButton = super.grabElement(nextButtonSelector);
         const previousSlideButton = super.grabElement(previousButtonSelector);
-        nextSlideButton.addEventListener('click',()=>this.nextSlide());
-        previousSlideButton.addEventListener('click',()=>this.previousSlide());
-       
+        nextSlideButton.addEventListener('click',()=>this.handleNextSlide());
+        previousSlideButton.addEventListener('click',()=>this.handlePreviousSlide());
+        this.autoSlide();
+    }
+
+    handleNextSlide(){
+       clearInterval(intervalIndex);
+       canAutomaticallyChange = false;
+       this.nextSlide();
+      
+       if(!canAutomaticallyChange){
+        setTimeout(()=>this.autoSlide(),5000)
+       }
+    }
+
+    handlePreviousSlide(){
+        clearInterval(intervalIndex);
+        canAutomaticallyChange = false; 
+        this.previousSlide();
+
+        if(!canAutomaticallyChange){
+        setTimeout(()=>this.autoSlide(),4000)
+        }
     }
 
     nextSlide(){
@@ -32,9 +56,18 @@ class Slider extends Common{
         this.element.style.backgroundImage = `url(${imagesToSlide[index]})`;
     }
 
+    autoSlide(){
+        clearInterval(intervalIndex);
+        intervalIndex = setInterval(()=>this.nextSlide(),4000);
+    }
 }
 
-export const slider = new Slider();
+window.addEventListener('DOMContentLoaded', ()=>{
+    const slider = new Slider();
+})
+
+
+
 
 
 
